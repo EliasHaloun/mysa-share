c0ed5ebac8aeeec0d8db41a732ba83e2c33a7769
EliasHaloun <elias.z.haloun@gmail.com>
2025-04-22 12:11:47 +0000

    Clean and edit

--- MysaShare/TheComponent.tsx
+++ MysaShare/TheComponent.tsx
@@ -1,4 +1,7 @@
+import { IonApp, IonContent, IonPage } from '@ionic/react';
 import React from 'react';
+import './TheStyle.css';
+
 
 export const Title: React.FC<{ text: string }> = ({ text }) => (
    <h1 style={{ fontFamily: "MainFont", color: 'purple', fontSize: '3rem', textAlign: 'center' }}>
@@ -10,4 +13,19 @@ export const Subtitle: React.FC<{ text: string }> = ({ text }) => (
    <h2 style={{ fontFamily: "MainFont", color: 'purple', fontSize: '2rem', textAlign: 'center' }}>
        {text}
    </h2>
+);
+
+export const MainContainer: React.FC<{ children: React.ReactNode }> = ({ children }) => (
+    <IonApp>
+        <IonPage>
+            <IonContent fullscreen>
+                <div style={{
+                    display: 'flex', flexDirection: 'column',
+                    alignItems: 'center', justifyContent: 'center', textAlign: 'center', placeContent: 'center'
+                }}>
+                    {children}
+                </div>
+            </IonContent>
+        </IonPage>
+    </IonApp>
 );
